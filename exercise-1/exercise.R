e9e65d79a1f64448a30755d4e2b8bb77624858ae
Ashish Israni <ashisrani@D-69-91-143-169.dhcp4.washington.edu>
2017-04-04 14:55:13 -0700

    changes made in class to module

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,10 +1,12 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-
-
+AddThree <- function(a) {
+  answer <- a + 3
+  return (answer)
+}
 # Create a variable `ten` by passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
 
